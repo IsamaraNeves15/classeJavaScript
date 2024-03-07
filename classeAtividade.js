f5c9a7beee9b4a9b9a331d39704d48c855e037e1
“isamaraneves15” <isamaraneves15@gmail.com>
2024-03-07 15:33:52 -0300

    atividade

--- classeAtividade.js
+++ classeAtividade.js
@@ -6,12 +6,41 @@ class Funcionario{
   }
 
   seApresentar(){
-  console.log("Olá, sou  " + this.nome + ", tenho " + this.idade + " anos, " + "e sou " + this.cargo)
+  console.log("Olá, sou  " + this.nome + ", tenho " + this.idade + " anos " + "e sou " + this.cargo)
   }
 
   trabalhar(){
-
+    console.log(this.nome + ", " + this.cargo)
+}
 }
 
-f1 = new Funcionario("André",27,"desenvolvedor")
-f1.seApresentar()
+class Gerente extends Funcionario{
+  constructor(nome,idade,cargo,departamento){
+    super(nome,idade,cargo);
+    this.departamento = departamento;
+  }
+
+    gerenciar(){
+      console.log("Sou " + this.cargo + " de " + this.departamento)
+    }
+}
+
+class Desenvolvedor extends Funcionario{
+  constructor(nome,idade,cargo,linguagem){
+    super(nome,idade,cargo);
+    this.linguagem = linguagem;
+  }
+
+  get programar(){
+    console.log("Eu programo em " + this.linguagem)
+  }
+}
+
+
+d1 = new Funcionario("André",27,"desenvolvedor","Phyton")
+d1.trabalhar()
+d1.programar()
+
+g1 = new Gerente("João",44, "gerente", "projetos")
+g1.seApresentar()
+g1.gerenciar()
